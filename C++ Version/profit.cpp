f2d7f0814c4cfc056fca9e9690cbac2ef5b630a0
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-06 08:05:58 -0500

    Added "Yes" Logic

--- C++ Version/profit.cpp
+++ C++ Version/profit.cpp
@@ -1,19 +1,24 @@
+// yes contract = 0
+// no contract = 1
+
 #include <iostream>
 
 int main(){
 
     bool capital;
-    bool yes_no_contract;
+    std::string yes_no_contract;
 
     //launcher
-    std::cout << "Inital Capital?: " << '\n'; //inital capital 
+    std::cout << "Inital Capital?: "; //inital capital 
     std::cin >> capital;
 
-    std::cout << "Purchased 'Yes' or 'No' Contract?"; //figures out if the user bought yes or no contract!
+    std::cout << "Purchased 'Yes' or 'No' Contract?: "; //figures out if the user bought yes or no contract!
     std::cin >> yes_no_contract;
     
+    if (yes_no_contract == "Yes"|| yes_no_contract == "YES" || yes_no_contract == "y" || yes_no_contract == "Y"){
+       int yes_no_contract = 0; //makes the var into a "Yes" contract
+    }
     
-
 
 
 
