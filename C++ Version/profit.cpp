6928f090f5c1901097e9c632b62682fb805bce20
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-06 08:26:56 -0500

    Updated Yes/No Contract implementation
    Reason I did this is because, I figured that it would be better to convert now (to a string) instead of doing extra steps later.

--- C++ Version/profit.cpp
+++ C++ Version/profit.cpp
@@ -1,5 +1,3 @@
-// yes contract = 0
-// no contract = 1
 
 #include <iostream>
 
@@ -18,12 +16,12 @@ int main(){
     
     // Figures out if the contract is a "Yes" or "No" 
     if (yes_no_contract == "Yes"|| yes_no_contract == "YES" || yes_no_contract == "y" || yes_no_contract == "Y"){
-       int yes_no_contract = 0; //makes the var into a "Yes" contract
+       std::string yes_no_contract = "Yes"
        
     }
     
     else if (yes_no_contract == "No" || yes_no_contract == "NO" || yes_no_contract == "n" || yes_no_contract == "N"){
-        int yes_no_contract = 1; //makes the var into a "No contract"
+        std::string yes_no_contract = "No"; 
     }
 
     else{
